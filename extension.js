18b6ecb30f0864bed9b44624db3342632a45ef14
Warley Rodrigues de Moura <warleydev@hotmail.com>
2026-04-07 05:55:42 -0300

    save

--- extension.js
+++ extension.js
@@ -1,52 +1,7 @@
-const vscode = require('vscode');
-
-function activate(context) {
-
-    const provider = vscode.languages.registerDocumentFormattingEditProvider('baseons', {
-        provideDocumentFormattingEdits(document) {
-
-            const fullText = document.getText();
-
-            const formatted = formatHTMLLike(fullText);
-
-            const fullRange = new vscode.Range(
-                document.positionAt(0),
-                document.positionAt(fullText.length)
-            );
-
-            return [vscode.TextEdit.replace(fullRange, formatted)];
-        }
-    });
-
-    context.subscriptions.push(provider);
-}
+const prettier = require("prettier");
 
 function formatHTMLLike(text) {
-    let indent = 0;
-    const lines = text.split('\n');
-
-    return lines.map(line => {
-        let trimmed = line.trim();
-
-        // fecha tag diminui indent
-        if (trimmed.match(/^<\/.+>/)) {
-            indent--;
-        }
-
-        let result = '  '.repeat(Math.max(indent, 0)) + trimmed;
-
-        // abre tag aumenta indent
-        if (trimmed.match(/^<[^\/!][^>]*[^\/]>$/)) {
-            indent++;
-        }
-
-        return result;
-    }).join('\n');
+    return prettier.format(text, {
+        parser: "html"
+    });
 }
-
-function deactivate() { }
-
-module.exports = {
-    activate,
-    deactivate
-};
